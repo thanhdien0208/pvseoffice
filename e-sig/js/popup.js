d13f16c09abc3ba18fa2a6fb875e1c41d4395a86
thanhdien0208 <diennguyen.blog@gmail.com>
2021-08-01 13:53:14 +0700

    upload

---
+++ e-sig/js/popup.js
@@ -0,0 +1,17 @@
+let modalBtn = document.getElementById("popup-btn");
+let modal = document.querySelector(".popup");
+let closeBtn = document.querySelector(".close-btn");
+// Hiển thị popup khi nhấp chuột vào button
+modalBtn.onclick = function(){
+modal.style.display = "block"
+}
+// Đóng popup khi ấn vào nút đóng
+closeBtn.onclick = function(){
+modal.style.display = "none"
+}
+// Đóng khi nhấp chuột vào bất cứ khu vực nào trên màn hình
+window.onclick = function(e){
+    if(e.target == popup){
+    modal.style.display = "none"
+    }
+    }
